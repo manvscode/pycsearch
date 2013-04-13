121be7e3be05691abd603174f15d82c960d36930
Joe Marrero <manvscode@gmail.com>
2013-04-13 01:16:41 -0400

    Somewhat working. I still need to debug the successors function since it is giving wrong transforms.

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -1,21 +1,23 @@
 import math
 import random
 import ctypes
+import sys
 from csearch import AStarSearch
+from csearch import Successors
 
 BOARD_WIDTH  = 3
 BOARD_HEIGHT = 3
 
-puzzle_state = ctypes.c_int * (BOARD_HEIGHT * BOARD_WIDTH)
-
-GOAL_STATE = puzzle_state(
+GOAL_STATE = [
 	1, 2, 3,
 	4, 5, 6,
 	7, 8, 0
-)
+]
 
 # A collection of all of the game board
 # states that was a possibility.
+# This is used to ensure that the GC
+# doesn't take our references away from us.
 states = []
 
 # Generate a random game board.
@@ -24,9 +26,9 @@ def randomize_board( board, width, height, only_solvable ):
 		# This may or may not produce a board with a solution.
 		length = width * height
 
-		numbers = puzzle_state([ i for x in range(0, length) ])
+		numbers = [ i for x in range(0, length) ]
 
-		for i in range(len - 1, 0, -1):
+		for i in range(length - 1, 0, -1):
 			random_index = random.randint( 0, i + 1)
 			chosen = numbers[ random_index ]
 
@@ -34,7 +36,7 @@ def randomize_board( board, width, height, only_solvable ):
 			board[ i ] = chosen
 	else:
 		# Start with the goal
-		current_board = (GOAL_STATE)
+		current_board = list(GOAL_STATE)
 
 		potential_moves = [
 			( -1,  0 ),
@@ -45,7 +47,7 @@ def randomize_board( board, width, height, only_solvable ):
 
 		# Attempt to make 14 moves to scramble the goal state.
 		# This new state will be the start state.
-		moves = 14;
+		moves = 2
 		while moves > 0:
 			moves -= 1
 
@@ -53,7 +55,7 @@ def randomize_board( board, width, height, only_solvable ):
 				for x in range(0, width):
 					index = width * y + x
 
-					# loop until you find the empty space to make moves 
+					# loop until you find the empty space to make moves
 					if current_board[ index ] == 0:
 						moved = False
 
@@ -74,12 +76,12 @@ def randomize_board( board, width, height, only_solvable ):
 
 		# copy randomized board to the game board
 		length = width * height
-		for i in range(0, length): 
+		for i in range(0, length):
 			board[ i ] = current_board[ i ]
 
-# Create a new game board state.
-def create_state( board, size, index, move_index ):
-	new_board = puzzle_state(board)
+# Create a new game board.
+def create_board( board, index, move_index ):
+	new_board = list(board)
 
 	tmp = new_board[ index ]
 	new_board[ index ] = new_board[ move_index ]
@@ -88,12 +90,14 @@ def create_state( board, size, index, move_index ):
 	states.append( new_board )
 	return new_board
 
-# Given a game board state, what are all of the possible
+# Given a game board, what are all of the possible
 # game boards that can result from all of the potential
-# moves. 
-def get_possible_moves( state, successors ):
-	current_board = puzzle_state( ctypes.cast( state, ctypes.c_long_p ).contents )
-	print current_board
+# moves.
+def get_possible_moves( current_board, successors_handle ):
+	successors = Successors(successors_handle)
+
+	print "Finding successors of:"
+	debug_board( current_board )
 
 	potential_moves = [
 		( -1,  0 ),
@@ -117,54 +121,52 @@ def get_possible_moves( state, successors ):
 				emptyX = x
 				emptyY = y
 
+	assert( found_empty )
+
 	if found_empty:
-		for m in range(0, 3):
+		for m in range(0, 4):
 			move_x = emptyX + potential_moves[ m ][ 0 ]
 			move_y = emptyY + potential_moves[ m ][ 1 ]
 
 			if move_x >= 0 and move_x < BOARD_WIDTH and move_y >= 0 and move_y < BOARD_HEIGHT:
 				# make the move
 				move_index = BOARD_WIDTH * move_y + move_x
+				print "empty = ({},{}), index = {}".format(emptyX, emptyY, BOARD_WIDTH * emptyY + emptyX)
+				print "move = ({},{}),  index = {}".format( move_x, move_y, move_index )
+				new_board = create_board( current_board, index, move_index )
 
-				new_state = create_state( current_board, BOARD_WIDTH * BOARD_HEIGHT, index, move_index )
+				print "Successor"
+				debug_board( new_board )
+				successors.push( new_board )
 
-				successorsPush( successors, new_state )
+	sys.stdin.read(1)
 
-# Draw a game board state. Step 0 implies no moves have
-# occurred and is our initial state.
+# Draw a game board board. Step 0 implies no moves have
+# occurred and is our initial board.
 def draw_board( step, board ):
-	for y in range(0, BOARD_HEIGHT - 1):
+	for y in range(0, BOARD_HEIGHT):
 		if y == 1:
 			if step == 0:
-				print ' {0:10}     '.format("Initial")
+				print ' {0:10} {1:3} |{2:1d}|{3:1d}|{4:1d}|'.format( "Initial", "",board[ BOARD_WIDTH * y + 0 ],  board[ BOARD_WIDTH * y + 1 ],  board[ BOARD_WIDTH * y + 2 ] );
 			else:
-				print ' {0:10} {1:3d} '.format("Step", step)
+				print ' {0:10} {1:3} |{2:1d}|{3:1d}|{4:1d}|'.format( "Step", step, board[ BOARD_WIDTH * y + 0 ],  board[ BOARD_WIDTH * y + 1 ],  board[ BOARD_WIDTH * y + 2 ] );
 		else:
-			print '                '
+				print ' {0:10} {1:3} |{2:1d}|{3:1d}|{4:1d}|'.format( "", "",       board[ BOARD_WIDTH * y + 0 ],  board[ BOARD_WIDTH * y + 1 ],  board[ BOARD_WIDTH * y + 2 ] );
 
-		for x in range(0, BOARD_WIDTH - 1):
-			index = BOARD_WIDTH * y + x
-			num   = board[ index ]
+	print ""
 
-			if num:
-				print '|{0:1d}'.format(num)
-			else:
-				print '| ' 
+def debug_board( board ):
+	print ' |{0:1d}|{1:1d}|{2:1d}|'.format( board[ 0 ],  board[ 1 ],  board[ 2 ] );
+	print ' |{0:1d}|{1:1d}|{2:1d}|'.format( board[ 3 ],  board[ 4 ],  board[ 5 ] );
+	print ' |{0:1d}|{1:1d}|{2:1d}|'.format( board[ 6 ],  board[ 7 ],  board[ 8 ] );
 
-		print '|\n'
+	print ""
+
 
 # The sum of the manhattan distance of each number between
 # game boards.
-def heuristic( state1, state2 ):
-	board1 = puzzle_state(state1)
-
-	draw_board( 0,  state1 )
-
-	board2 = puzzle_state(state2)
-	#board1 = state1[0]
-	#board2 = state2[0]
-
-	sum = 0;
+def heuristic( board1, board2 ):
+	sum = 0
 
 	for y1 in range(0, BOARD_HEIGHT):
 		for x1 in range(0, BOARD_WIDTH):
@@ -179,21 +181,21 @@ def heuristic( state1, state2 ):
 	return sum
 
 # The cost of making a move is 1.
-def cost( state1, state2 ):
+def cost( board1, board2 ):
 	return 1
 
-# Two game board states are equal if every number is in the same 
+# Two game board states are equal if every number is in the same
 # position in both.
-def board_compare( state1, state2 ):
-	if state1 == state2:
-		return 0
-	elif state1 < state2:
-		return -1
-	else:
-		return 1
+def board_compare( board1, board2 ):
+	for i in range(0, BOARD_WIDTH * BOARD_HEIGHT):
+		if board1[ i ] != board2[ i ]:
+			return 1
 
-def state_hash(state):
-	return hash(state)
+	return 0
+
+
+def state_hash(board):
+	return hash(tuple(board))
 
 def main():
 	random.seed( None )
@@ -201,28 +203,27 @@ def main():
 	astar = AStarSearch( board_compare, state_hash, heuristic, cost, get_possible_moves )
 
 	# Produce a solvable random board
-	initial_state = puzzle_state()
+	initial_state = list(GOAL_STATE)
 	randomize_board( initial_state, BOARD_WIDTH, BOARD_HEIGHT, True )
+
 	states.append( initial_state )
 
 	if astar.find( GOAL_STATE, initial_state ):
 		step = 0;
-		state = astar.firstState()
-		while state != None:
-			board = puzzle_state(state)
+		board = astar.firstState()
+		while board != None:
 
 			draw_board( step, board )
 			print "\n"
-			
+
 			step += 1
-			state = astar.nextState()
+			board = astar.nextState()
 
 		astar.cleanup( )
 	else:
-		# No solution found. 
+		# No solution found.
 		print "No solution found for:\n\n"
 		draw_board( 0, initial_state );
 
-
 main()
-	
+
--- csearch.py
+++ csearch.py
@@ -1,119 +1,137 @@
 from ctypes import *
 from ctypes.util import *
 
-libcsearch_file = find_library('csearch')
-libcsearch      = CDLL( libcsearch_file )
-
-#typedef void         (*successors_fxn)         ( const void* __restrict state, successors_t* __restrict p_successors )
+lib_file = find_library('csearch')
+lib      = CDLL( lib_file )
 
 
-to_state_hash_fxn       = CFUNCTYPE( c_size_t, c_void_p )
-to_compare_fxn          = CFUNCTYPE( c_int, c_void_p, c_void_p )
-to_heuristic_fxn        = CFUNCTYPE( c_int, c_void_p, c_void_p )
-to_cost_fxn             = CFUNCTYPE( c_int, c_void_p, c_void_p )
-to_nonnegative_cost_fxn = CFUNCTYPE( c_uint, c_void_p, c_void_p )
-to_successors_fxn       = CFUNCTYPE( None, c_void_p, c_void_p ) 
+state_hash_fxn       = CFUNCTYPE( c_size_t, py_object )
+compare_fxn          = CFUNCTYPE( c_int, py_object, py_object )
+heuristic_fxn        = CFUNCTYPE( c_int, py_object, py_object )
+cost_fxn             = CFUNCTYPE( c_int, py_object, py_object )
+nonnegative_cost_fxn = CFUNCTYPE( c_uint, py_object, py_object )
+successors_fxn       = CFUNCTYPE( None, py_object, c_void_p )
 
-#print dir( )
+class Successors:
+	handle = None
 
-def successorsPush( successors, state ):
-	return libcsearch.successors_push( successors, byref(state) )
+	def __init__( self, handle ):
+		self.handle = handle
 
-def successorsPop( successors ):
-	return libcsearch.successors_pop( successors )
+	def push( self, state ):
+		lib.successors_push.restype  = c_bool
+		lib.successors_push.argtypes = [c_void_p, py_object]
+		return lib.successors_push( self.handle, py_object(state) )
 
-def successorsResize( successors, new_size ):
-	return libcsearch.successors_resize( successors, new_size )
+	def pop( self ):
+		lib.successors_pop.restype  = c_bool
+		lib.successors_pop.argtypes = [c_void_p]
+		return lib.successors_pop( self.handle )
 
-def successorsClear( successors ):
-	return libcsearch.successors_clear( successors )
+	def resize( self, new_size ):
+		lib.successors_resize.restype  = c_bool
+		lib.successors_resize.argtypes = [c_void_p, c_uint]
+		return lib.successors_resize( self.handle, new_size )
 
-
-class BestFirstSearch:
-	bfs = None
-	node = None
-
-	def __init__( self, compare_fxn, state_hasher, heuristic, successors_of ):
-		self.bfs = libcsearch.bestfs_create( to_compare_fxn(compare_fxn), to_state_hash_fxn(state_hasher), to_heuristic_fxn(heuristic), to_successors_of(successors_of) )
-
-	def __deinit__( self ):
-		libcsearch.bestfs_destroy( self.bfs )
-
-	def setCompareFxn( self, compare_fxn ):
-		libcsearch.bestfs_set_compare_fxn( self.bfs, to_compare_fxn(compare_fxn) )
-
-	def setHeuristicFxn( self, heuristic_fxn ):
-		libcsearch.bestfs_set_heuristic_fxn( self.bfs, to_heuristic_fxn(heuristic_fxn) )
-
-	def setSuccessorsFxn( self, successors_fxn ):
-		libcsearch.bestfs_set_successors_fxn( self.bfs, to_successors_fxn(successors_fxn) )
-
-	def cleanup( self ):
-		libcsearch.bestfs_cleanup( self.bfs )
-
-	def find( self, start, end ):
-		return libcsearch.bestfs_find( self.bfs, byref(start), byref(end) )
-
-	def firstState( self ):
-		node = libcsearch.bestfs_first_node( self.bfs )
-		return libcsearch.bestfs_state( self.node )
-
-	def nextState( self ):
-		node = libcsearch.bestfs_next_node( self.node )
-		return libcsearch.bestfs_state( self.node )
-
-	def iterativeInit( self, start, end, found ):
-		return libcsearch.bestfs_iterative_init( self.bfs, byref(start), byref(end), byref(found) )	
-
-	def iterativeFind( self, start, end, found ):
-		return libcsearch.bestfs_iterative_find( self.bfs, byref(start), byref(end), byref(found) )	
-
-	def iterativeIsDone( self, found ):
-		return libcsearch.bestfs_iterative_is_done( self.bfs, byref(found) )
+	def clear( self ):
+		lib.successors_clear.restype  = None
+		lib.successors_clear.argtypes = [c_void_p]
+		return lib.successors_clear( self.handle )
 
 class AStarSearch:
-	astar = None
+	handle = None
 	node = None
+	compare = None
+	state_hasher = None
+	heuristic = None
+	cost = None
+	successors_of = None
 
-	def __init__( self, compare_fxn, state_hasher, heuristic, cost, successors_of ):
-		self.astar = libcsearch.astar_create( to_compare_fxn(compare_fxn), to_state_hash_fxn(state_hasher), to_heuristic_fxn(heuristic), to_cost_fxn(cost), to_successors_fxn(successors_of) )
+	def __init__( self, compare, state_hasher, heuristic, cost, successors_of ):
+		self.compare       = compare_fxn(compare)
+		self.state_hasher  = state_hash_fxn(state_hasher)
+		self.heuristic     = heuristic_fxn(heuristic)
+		self.cost          = cost_fxn(cost)
+		self.successors_of = successors_fxn(successors_of)
+
+		lib.astar_create.restype  = c_void_p
+		lib.astar_create.argtypes = [compare_fxn, state_hash_fxn, heuristic_fxn, cost_fxn, successors_fxn]
+		self.handle = lib.astar_create( self.compare, self.state_hasher, self.heuristic, self.cost, self.successors_of )
 
 	def __deinit__( self ):
-		libcsearch.astar_destroy( self.astar )
+		lib.astar_destroy.restype  = None
+		lib.astar_destroy.argtypes = [c_void_p]
+		lib.astar_destroy( self.handle )
 
-	def setCompareFxn( self, compare_fxn ):
-		libcsearch.astar_set_compare_fxn( self.astar, to_compare_fxn(compare_fxn) )
+	def setCompareFunction( self, fxn ):
+		self.compare = compare_fxn(fxn)
+		lib.astar_set_compare_fxn.restype = None
+		lib.astar_set_compare_fxn.argtypes = [c_void_p, compare_fxn]
+		lib.astar_set_compare_fxn( self.handle, self.compare )
 
-	def setHeuristicFxn( self, heuristic_fxn ):
-		libcsearch.astar_set_heuristic_fxn( self.astar, to_heuristic_fxn(heuristic_fxn) )
+	def setHeuristicFunction( self, fxn ):
+		self.state_hasher = state_hash_fxn(fxn)
+		lib.astar_set_heuristic_fxn.restype = None
+		lib.astar_set_heuristic_fxn.argtypes = [c_void_p, heuristic_fxn]
+		lib.astar_set_heuristic_fxn( self.handle, self.state_hasher )
 
-	def setCostFxn( self, cost_fxn ):
-		libcsearch.astar_set_cost_fxn( self.astar, to_cost_fxn(cost_fxn) )
+	def setCostFunction( self, fxn ):
+		self.cost = cost_fxn(fxn)
+		lib.astar_set_cost_fxn.restype = None
+		lib.astar_set_cost_fxn.argtypes = [c_void_p, compare_fxn]
+		lib.astar_set_cost_fxn( self.handle, self.cost )
 
-	def setSuccessorsFxn( self, successors_fxn ):
-		libcsearch.astar_set_successors_fxn( self.astar, to_successors_fxn(successors_fxn) )
+	def setSuccessorsFunction( self, fxn ):
+		self.successors_of = successors_fxn(fxn)
+		lib.astar_set_successors_fxn.restype = None
+		lib.astar_set_successors_fxn.argtypes = [c_void_p, successors_fxn]
+		lib.astar_set_successors_fxn( self.handle, self.successors_of )
 
 	def cleanup( self ):
-		libcsearch.astar_cleanup( self.astar )
+		lib.astar_cleanup.restype  = None
+		lib.astar_cleanup.argtypes = [c_void_p]
+		lib.astar_cleanup( self.handle )
 
 	def find( self, start, end ):
-		return libcsearch.astar_find( self.astar, byref(start), byref(end) )
+		lib.astar_find.restype  = c_bool
+		lib.astar_find.argtypes = [c_void_p, py_object, py_object]
+		return lib.astar_find( self.handle, py_object(start), py_object(end) )
 
 	def firstState( self ):
-		node = libcsearch.astar_first_node( self.astar )
-		return libcsearch.astar_state( self.node )
+		lib.astar_first_node.restype  = c_void_p
+		lib.astar_first_node.argtypes = [c_void_p]
+		self.node = lib.astar_first_node( self.handle )
+		state = None
+		if self.node != None:
+			lib.astar_state.restype  = py_object
+			lib.astar_state.argtypes = [c_void_p]
+			state = lib.astar_state( self.node )
+		return state
 
 	def nextState( self ):
-		node = libcsearch.astar_next_node( self.node )
-		return libcsearch.astar_state( self.node )
+		lib.astar_next_node.restype  = c_void_p
+		lib.astar_next_node.argtypes = [c_void_p]
+		self.node = lib.astar_next_node( self.node )
+		state = None
+		if self.node != None:
+			lib.astar_state.restype  = py_object
+			lib.astar_state.argtypes = [c_void_p]
+			state = lib.astar_state( self.node )
+		return state
 
 	def iterativeInit( self, start, end, found ):
-		return libcsearch.astar_iterative_init( self.astar, byref(start), byref(end), byref(found) )	
+		lib.astar_iterative_init.restype  = None
+		lib.astar_iterative_init.argtypes = [c_void_p, py_object, py_object, py_object]
+		return lib.astar_iterative_init( self.handle, py_object(start), py_object(end), py_object(found) )
 
 	def iterativeFind( self, start, end, found ):
-		return libcsearch.astar_iterative_find( self.astar, byref(start), byref(end), byref(found) )	
+		lib.astar_iterative_find.restype  = None
+		lib.astar_iterative_find.argtypes = [c_void_p, py_object, py_object, py_object]
+		return lib.astar_iterative_find( self.handle, py_object(start), py_object(end), py_object(found) )
 
 	def iterativeIsDone( self, found ):
-		return libcsearch.astar_iterative_is_done( self.astar, byref(found) )
+		lib.astar_iterative_is_done.restype  = c_bool
+		lib.astar_iterative_is_done.argtypes = [c_void_p, py_object]
+		return lib.astar_iterative_is_done( self.handle, py_object(found) )
 
 
